b7e75160befa4433f9073eda21050abd357e3afc
AgeOfWar <palazzomichi@gmail.com>
2018-01-24 17:03:49 +0100

    Added arguments handling.

--- src/main/kotlin/me/palazzomichi/brainfuckinterpreter/main.kt
+++ src/main/kotlin/me/palazzomichi/brainfuckinterpreter/main.kt
@@ -3,14 +3,38 @@ package me.palazzomichi.brainfuckinterpreter
 import me.palazzomichi.brainfuckinterpreter.util.execute
 import java.io.FileReader
 
-fun main(args: Array<String>) {
-    if (args.size != 1) {
-        error("invalid arguments")
-    }
-    
-    val path = args[0]
-    val reader = FileReader(path)
+fun main(args: Array<String>) = when (args.size) {
+    1 -> main(args[0], "-f")
+    2 -> main(args[1], args[0])
+    else -> main()
+}
+
+fun main() {
+    println("""
+        Usage: [-fsi] <program>
+        -f --file       run brainfuck file
+        -s --string     run code directly
+        -i --infinite   run without cell limits
+        """.trimIndent())
+}
+
+fun main(arg: String, option: String) = when (option) {
+    "-f", "--file" -> main(FileReader(arg))
+    "-s", "--string" -> main(arg)
+    "-i", "--infinite",
+    "-fi", "-if" -> main(FileReader(arg), CellIterator.infinite())
+    "-si", "-is" -> main(arg, CellIterator.infinite())
+    else -> main()
+}
+
+fun main(program: FileReader, cellIterator: CellIterator = CellIterator.default()) {
     val brainfuck = BrainfuckInterpreter(System.`in`, System.out)
-    brainfuck.execute(reader)
+    brainfuck.execute(program, cellIterator)
+    System.out.flush()
+}
+
+fun main(program: String, cellIterator: CellIterator = CellIterator.default()) {
+    val brainfuck = BrainfuckInterpreter(System.`in`, System.out)
+    brainfuck.execute(program, cellIterator)
     System.out.flush()
 }
